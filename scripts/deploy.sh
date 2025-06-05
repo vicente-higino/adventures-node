83a76ec9fc9d7cb2b73b0043feb9c0e0c2595d8b
Vicente Higino <vi_higino@hotmail.com.br>
2025-06-05 19:34:22 -0300

    feat: update deploy script to improve logging and usage instructions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Example usage:
+# REPO_PATH=/path/to/repo bash deploy.sh
+# or when using ssh:
+# ssh user@remote_host 'REPO_PATH=/path/to/repo bash -s' < deploy.sh
+
 
 cd ${REPO_PATH};
 docker compose up -d --force-recreate --pull always "server";
 docker image prune -a -f;
 docker ps;
 # docker compose restart "server";
-sleep 10;
-docker compose logs "server";
+# sleep 10;
+docker compose logs -f "server";
 exit;
