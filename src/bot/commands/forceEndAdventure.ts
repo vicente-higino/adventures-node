ea166b17e969cf7831b580bae3027d8838e63e5b
Vicente Higino <vi_higino@hotmail.com.br>
2025-10-26 02:18:11 -0300

    feat(adventure): enhance forceEndAdventureCommand to include broadcaster context and improve adventure ending logic

--- src/bot/commands/forceEndAdventure.ts
+++ src/bot/commands/forceEndAdventure.ts
@@ -1,12 +1,24 @@
 import { getBotConfig } from "@/bot";
 import { createBotCommand } from "../BotCommandWithKeywords";
+import { handleAdventureEnd } from "@/common/handleAdventure";
 
 export const forceEndAdventureCommand = createBotCommand(
     "forceendadventure",
     async (params, ctx) => {
-        let { say, msg } = ctx;
-        const { isMod, isBroadcaster, userId } = msg.userInfo;
+        let { say, msg, broadcasterName, broadcasterId } = ctx;
+        const { isMod, isBroadcaster, userId, userName, displayName } = msg.userInfo;
         if (!isMod && !isBroadcaster && getBotConfig().superUserId !== userId) {
+            return;
+        }
+        if (getBotConfig().modChannels.includes(broadcasterName)) {
+            const res = await handleAdventureEnd({
+                channelLogin: broadcasterName,
+                channelProviderId: broadcasterId,
+                userProviderId: getBotConfig().userId,
+                userLogin: userName,
+                userDisplayName: displayName,
+            })
+            say(res);
             return;
         }
         say(`!adventureend`);
