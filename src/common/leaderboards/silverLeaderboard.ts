75dd2f708bd5846eb068d4a87bf10de79129f2e7
Vicente Higino <vi_higino@hotmail.com.br>
2025-11-17 08:15:38 -0300

    fix(silver leaderboard): sort the name as well

--- src/common/leaderboards/silverLeaderboard.ts
+++ src/common/leaderboards/silverLeaderboard.ts
@@ -10,20 +10,21 @@ export async function handleSilver(
 ): Promise<LeaderboardResult> {
     const balances = await prisma.balance.findMany({
         where: { channelProviderId: channelProviderId },
-        include: { user: { select: { displayName: true } } },
+        include: { user: { select: { displayName: true, login: true } } }
     });
     const totalEntries = balances.length;
 
     const formattedLeaderboard = balances
-        .map(entry => ({ name: entry.user.displayName, value: entry.value }))
+        .map(entry => ({ name: entry.user.login, displayName: entry.user.displayName, value: entry.value }))
         .sort((a, b) => {
             const multiplier = order === "asc" ? 1 : -1;
-            return (a.value - b.value) * multiplier;
+            const nameSort = a.name.localeCompare(b.name);
+            return (a.value - b.value + nameSort) * multiplier;
         })
         .slice(0, amount)
         .map((entry, i) => {
             const index = order === "asc" ? totalEntries - i : i + 1;
-            return `${index}. ${entry.name}: ${formatSilver(entry.value)} Silver`;
+            return `${index}. ${entry.displayName}: ${formatSilver(entry.value)} Silver`;
         });
 
     return { formattedLeaderboard, metricDisplay: "silver", leaderboardType: "Silver" };
