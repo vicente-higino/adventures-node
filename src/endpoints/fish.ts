436ba0d6595686c51446c4ec1a68881daf73b0b0
Vicente Higino <vi_higino@hotmail.com.br>
2025-11-26 21:28:24 -0300

    feat(fish): modify response to remove "/me " prefix from fishing results

--- src/endpoints/fish.ts
+++ src/endpoints/fish.ts
@@ -27,6 +27,6 @@ export class Fish extends OpenAPIRoute {
             cooldownHours: c.env.COOLDOWN_FISHING_IN_HOURS,
         });
 
-        return c.text(result);
+        return c.text(result.replace("/me ", ""));
     }
 }
