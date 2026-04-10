7b08f9d808c162f3444061005e533390b9137858
Vicente Higino <vi_higino@hotmail.com.br>
2026-04-10 04:34:20 -0300

    feature(showAllEmotes): add options to filter missing emotes

--- src/bot/commands/showAllEmotes.ts
+++ src/bot/commands/showAllEmotes.ts
@@ -1,19 +1,32 @@
 import { createBotCommand } from "../BotCommandWithKeywords";
-import { getBotConfig } from "@/bot";
-import * as emotes from "@/emotes";
+import { emoteTracker, getBotConfig } from "@/bot";
+import { EmoteManager } from "@/emotes";
+import { z } from "zod";
+
+type Option = "all" | "missing";
+
+const parseOption = z.enum(["all", "missing"]);
 
 export const showAllEmotesCommand = createBotCommand(
     "showusedemotes",
     async (params, ctx) => {
-        const { say, msg } = ctx;
+        const { say, msg, broadcasterName } = ctx;
         const { isMod, isBroadcaster, userId } = msg.userInfo;
         if (!isMod && !isBroadcaster && getBotConfig().superUserId !== userId) {
             return;
         }
-        // Gather all emote arrays from emotes.ts
-        const allEmotes = emotes.EmoteManager.getAllEmotes().map(e => e.name);
+        const arg = parseOption.safeParse(params.shift());
+        let option: Option = 'all';
+        if (arg.success) {
+            option = arg.data;
+        }
+        let emotes = EmoteManager.getAllEmotes();
+        if (option == 'missing') {
+            emotes = emotes.filter(e => !emoteTracker?.channelHasEmote(broadcasterName, e.name) && e.provider !== 'native')
+        }
+        const allEmotes = emotes.map(e => e.name);
         // Remove duplicates and sort
-        const uniqueEmotes = Array.from(new Set(allEmotes));
+        const uniqueEmotes = Array.from(new Set(allEmotes)).toSorted();
         say(uniqueEmotes.join(" "));
     },
     { aliases: ["sue"], ignoreCase: true },
