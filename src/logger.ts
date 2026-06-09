8330ced9eee381456b52128797565ef24f063972
Vicente Higino <vi_higino@hotmail.com.br>
2026-06-09 10:18:58 -0300

    fix(logger): improve logger configuration for development environment

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,9 @@
 import pino from "pino";
+import pretty from "pino-pretty";
 
-const logger = pino({ level: process.env.LOG_LEVEL || "info", transport: { target: "pino-pretty", options: { colorize: true } } });
+const logger =
+    process.env.NODE_ENV === "development"
+        ? pino({ level: process.env.LOG_LEVEL || "info" }, pretty({ colorize: true, singleLine: false }))
+        : pino({ level: process.env.LOG_LEVEL || "info", transport: { target: "pino-pretty", options: { colorize: true } } });
+
 export default logger;
